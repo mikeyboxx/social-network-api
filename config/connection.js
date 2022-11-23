863729d1b014007ab73b33f4041c9be429bb1d23
Michael Nabatov <happyanddebtfree@gmail.com>
2022-11-23 16:00:14 -0500

    push to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // Wrap Mongoose around local connection to MongoDB
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social_network_db',
   {
     useNewUrlParser: true,
